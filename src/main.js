4261b91bb7bb1d2463b24af194a6481c41b3c883
vismay3175 <vismay3175@gmail.com>
2026-04-18 02:23:30 +0530

    Add Lord Ganesha blessings to initial loader

--- src/main.js
+++ src/main.js
@@ -96,6 +96,16 @@ function initLoader(callback) {
     }
   });
 
+  const ganesha = document.querySelector('.loader-ganesha');
+  if (ganesha) {
+    tl.to(ganesha, {
+      opacity: 1,
+      y: 0,
+      duration: 1.2,
+      ease: 'power3.out'
+    });
+  }
+
   tl.from(chars, {
     y: 50,
     opacity: 0,
